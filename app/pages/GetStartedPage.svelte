971f5df8ab2cbac588ea3e426b689b60847e82a4
Anjolaoluwa Akindipe <anjyakindipe@gmail.com>
2022-02-15 16:15:19 +0100

    Adding comments to the pages and functions

--- app/pages/GetStartedPage.svelte
+++ app/pages/GetStartedPage.svelte
@@ -1,46 +1,58 @@
 <page actionBarHidden= {true} class="background">
     <flexboxLayout alignItems="center">
-    <stackLayout  class=".container" >
-    <flexboxLayout>
-        <image src={"~/images/logo.png"}/>
-    </flexboxLayout>
-        <label class="text-light big-text " textWrap=true>
-            <formattedString>
-                <span text="Welcome to Island Microfinance Bank" />
-            </formattedString>
-        </label>
+        <stackLayout  class=".container" >
+            <!-- Logo on the top -->
+            <flexboxLayout>
+                <image src={"~/images/logo.png"}/>
+            </flexboxLayout>
 
-        <label class="text-light text-subinfo" textWrap=true>
-            <formattedString>
-                <span text="New level of Microfinance banking with our customer app" />
-            </formattedString>
-        </label>
-        <button class="btn text-bold" text="Get Started" on:tap={navigation}/>
-        <flexboxLayout justifyContent="center">
+            <!-- Header text -->
+            <label class="text-light big-text " textWrap=true>
+                <formattedString>
+                    <span text="Welcome to Island Microfinance Bank" />
+                </formattedString>
+            </label> 
+
+            <!-- Header sub-text -->
+            <label class="text-light text-subinfo" textWrap=true>
+                <formattedString>
+                    <span text="New level of Microfinance banking with our customer app" />
+                </formattedString>
+            </label>
+            
+            <!-- button for navigating to the NextGetStartedPage -->
+            <button class="btn text-bold" text="Get Started" on:tap={navigation}/>
+            
+            <flexboxLayout justifyContent="center">
+            
+                <stackLayout orientation="horizontal">
+                    <!-- under-button info text -->
+                    <label class="text-light under-info" textWrap=true>
+                        <formattedString>
+                            <span text="Have an account? " />
+                        </formattedString>
+                    </label>
+
+                    <!-- under-button info text to navigate to SignIn page -->
+                    <label class="text under-info" textWrap=true>
+                        <formattedString>
+                            <span style="color: green;" text="Sign In"   />
+                        </formattedString>
+                    </label>
         
-    <stackLayout orientation="horizontal">
-        
-        <label class="text-light under-info" textWrap=true>
-            <formattedString>
-                <span text="Have an account? " />
-            </formattedString>
-        </label>
-        <label class="text under-info" textWrap=true>
-            <formattedString>
-                <span style="color: green;" text="Sign In"   />
-            </formattedString>
-        </label>
-     
-    </stackLayout> 
-    </flexboxLayout> 
-    </stackLayout>
+                </stackLayout> 
+            </flexboxLayout> 
+        </stackLayout>
     </flexboxLayout>
 </page>
 
 <script>
-    import {navigate} from 'svelte-native'
+    // svelte imports
+    import {navigate} from 'svelte-native';
+
+    // page imports
     import NextGetStartedPage from './NextGetStartedPage.svelte';
-    import { Http } from '@nativescript/core';
+    
 
     const navigation =()=>navigate({page:NextGetStartedPage})  ;
 
